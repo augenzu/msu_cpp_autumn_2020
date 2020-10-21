8ebe42d0b2f0d4f42f459aa60a4bcebb8400ddd2
augenzu <elfokd@gmail.com>
2020-10-21 17:57:22 +0300

    02: Change tests

--- 02/test.cpp
+++ 02/test.cpp
@@ -1,71 +1,70 @@
 #include "parser.h"
 #include "test.h"
 
-std::vector<InOut> test_in_outs{
+// test inputs and true outputs
+std::vector<InOut> tests{
     { 
-        "  asd 1    32not-a-number", { { 1 }, 
-        { "asd", "32not-a-number" } } 
+        "  asd 1    32not-a-number", 
+        { { 0 }, { 0 }, { 1 }, { "asd", "32not-a-number" } } 
     },
     { 
         "000123\n\t0000123a", 
-        { { 123 }, { "0000123a" } } 
+        { { 0 }, { 0 }, { 123 }, { "0000123a" } } 
     },
     { 
         "\tword-word--word--- 42\\42\\42/", 
-        { {}, { "word-word--word---", "42\\42\\42/" } } 
+        { { 0 }, { 0 }, {}, { "word-word--word---", "42\\42\\42/" } } 
     },
     { 
         "very_very_long_string\t\r10_000_000\n\n\n\n\n\n", 
-        { {}, { "very_very_long_string", "10_000_000" } } 
+        { { 0 }, { 0 }, {}, { "very_very_long_string", "10_000_000" } } 
     },
     { 
         "---___---___8___--- 8 ---___---", 
-        { { 8 }, { "---___---___8___---", "---___---" } } 
+        { { 0 }, { 0 }, { 8 }, { "---___---___8___---", "---___---" } } 
     }
 };
 
 void
 testing()
 {
-    std::vector<std::string> start_v{};
-    std::vector<std::string> end_v{};
-    std::vector<uint64_t> num_v{};
-    std::vector<std::string> str_v{};
+    // for output of test callback
+    Output output{};
 
+    // test callbacks
     auto 
-    lambda_start_callback_test = [&]()
+    start_callback_test = [&]()
             {
-                start_v.push_back("LSCT");
+                output.start_v.push_back(0);
             };
     auto 
-    lambda_end_callback_test = [&]()
+    end_callback_test = [&]()
             {
-                end_v.push_back("LECT");
+                output.end_v.push_back(0);
             };
     auto 
-    lambda_number_token_callback_test = [&](uint64_t num)
+    number_token_callback_test = [&](uint64_t num)
             {
-                num_v.push_back(num);
+                output.num_v.push_back(num);
             };
     auto 
-    lambda_string_token_callback_test = [&](const std::string &str)
+    string_token_callback_test = [&](const std::string &str)
             {
-                str_v.push_back(str);
+                output.str_v.push_back(str);
             };
 
     TokenParser tp;
-    tp.set_start_callback(lambda_start_callback_test);
-    tp.set_end_callback(lambda_end_callback_test);
-    tp.set_number_token_callback(lambda_number_token_callback_test);
-    tp.set_string_token_callback(lambda_string_token_callback_test);
+    tp.set_start_callback(start_callback_test);
+    tp.set_end_callback(end_callback_test);
+    tp.set_number_token_callback(number_token_callback_test);
+    tp.set_string_token_callback(string_token_callback_test);
 
-    for (auto &test_in_out: test_in_outs) {
-        start_v = {};
-        end_v = {};
-        num_v = {};
-        str_v = {};
-        tp.parse(test_in_out.input);
-        assert(num_v == test_in_out.output.first);
-        assert(str_v == test_in_out.output.second);
+    for (auto &test: tests) {
+        output = {};
+        tp.parse(test.input);
+        assert(output.num_v == test.output.num_v);
+        assert(output.str_v == test.output.str_v);
     }
+
+    std::cout << "All tests passed successfully." << std::endl;
 }
--- 02/test.h
+++ 02/test.h
@@ -8,11 +8,22 @@
 #include <vector>
 #include "parser.h"
 
+// struct for possible results of test callback functions
+struct Output
+{
+    std::vector<int> start_v{};
+    std::vector<int> end_v{};
+    std::vector<uint64_t> num_v{};
+    std::vector<std::string> str_v{};
+};
+
+// parsing strings (inputs) and their corresponding results (outputs)
 struct InOut
 {
     std::string input{};
-    std::pair<std::vector<uint64_t>, std::vector<std::string>> output{};
+    Output output{};
 };
+
 extern std::vector<InOut> test_in_outs;
 
 // perform testing
