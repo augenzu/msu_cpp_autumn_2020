9776bee4be91a8bab6aeec75b3156d5b55293644
augenzu <elfokd@gmail.com>
2020-10-23 00:07:55 +0300

    03: Add right indexing tests

--- 03/test.cpp
+++ 03/test.cpp
@@ -1,14 +1,33 @@
+#include "matrix.h"
 #include "test.h"
 #include <iostream>
 
 void
 simple_create_test()
 {
-    return;
+    Matrix m;
+}
+
+void
+right_const_indexing_test()
+{
+    Matrix m(2, 3);
+    int i = m[0][0];
+    assert(i == m[0][0]);
+}
+
+void
+right_assign_indexing_test()
+{
+    Matrix m(2, 3);
+    m[1][2] = 1;
+    assert(m[1][2] == 1);
 }
 
 std::vector<std::function<void()>> tests{
-    simple_create_test
+    simple_create_test,
+    right_const_indexing_test,
+    right_assign_indexing_test
 };
 
 void
--- 03/test.h
+++ 03/test.h
@@ -8,6 +8,10 @@ extern std::vector<std::function<void()>> tests;
 
 void
 simple_create_test();
+void
+right_const_indexing_test();
+void
+right_assign_indexing_test();
 
 void
 testing();
