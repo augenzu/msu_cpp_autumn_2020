4385d4a59152577dfc3bc7678d06f5152e247c53
augenzu <elfokd@gmail.com>
2020-10-31 12:33:43 +0300

    04: Remove tests declarations

--- 04/test.h
+++ 04/test.h
@@ -7,21 +7,6 @@
 #include <sstream>
 #include <vector>
 
-extern const std::vector<std::function<void()>> tests;
-
-void
-simple_create_test();
-void
-ostream_operator_test();
-void
-equal_compare_test();
-void
-less_compare_test();
-void
-copy_constructor_test();
-void
-move_constructor_test();
-
 // perform testing
 void
 testing();
