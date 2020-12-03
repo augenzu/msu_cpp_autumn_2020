32f3f0963506ff618ac18d580ffaae403dce9060
augenzu <elfokd@gmail.com>
2020-12-03 13:44:00 +0300

    08: Rewrite everything

--- 08/thrpool.cpp
+++ 08/thrpool.cpp
@@ -1,6 +1,24 @@
 #include "thrpool.hpp"
 
 ThreadPool::ThreadPool(size_t pool_sz)
-: pool_sz_(pool_sz), running_(0), mtx_()
-{
+: pool_sz_(pool_sz), end_flag_{false}
+{        
+    for (size_t i = 0; i < pool_sz_; ++i) {
+        thrs_.emplace_back([this] 
+        {
+            executor(); 
+        });
+    }
+}
+
+ThreadPool::~ThreadPool()
+{       
+    {
+        std::unique_lock lock{mtx_};
+        end_flag_ = true;
+    }
+    condvar_.notify_all();
+    for (auto &thr: thrs_) {
+        thr.join();
+    }
 }
--- 08/thrpool.hpp
+++ 08/thrpool.hpp
@@ -1,27 +1,81 @@
 #ifndef THRPOOL_HPP
 #define THRPOOL_HPP
 
+#include <condition_variable>
 #include <iostream>
 #include <functional>
 #include <future>
 #include <mutex>
+#include <stdexcept>
 #include <thread>
+#include <queue>
+#include <vector>
 
 class ThreadPool
 {
 private:
     size_t pool_sz_;
-    size_t running_{};
+    std::vector<std::thread> thrs_{};
     std::mutex mtx_{};
+    std::queue<std::function<void()>> tasks_{};
+    std::condition_variable condvar_{};
+    bool end_flag_{false};
+
+    void
+    executor()
+    {
+        while (true) {
+            std::function<void()> task;
+
+            {
+                std::unique_lock lock{mtx_};
+                condvar_.wait(lock, [this]
+                { 
+                    return end_flag_ || !tasks_.empty(); 
+                });
+                if (end_flag_ && tasks_.empty()) {
+                    return;
+                }
+                task = std::move(tasks_.front());
+                tasks_.pop();
+            }
+
+            task();
+        }
+    }
 
 public:
     explicit ThreadPool(size_t);
+    ~ThreadPool();
 
     template <class Func, class ...Args>
     auto 
     exec(Func func, Args ...args) -> std::future<decltype(func(args...))>;
 };
 
-#include "thrpool.tpp"
+template <class Func, class ...Args>
+auto 
+ThreadPool::exec(Func func, Args ...args) -> std::future<decltype(func(args...))>
+{    
+    using ret_t = std::result_of_t<Func(Args...)>;
+    
+    auto task = std::make_shared<std::packaged_task<ret_t()>>(
+            std::bind(std::forward<Func>(func), std::forward<Args>(args)...));
+    std::future<ret_t> future = task->get_future();
+
+    {
+        std::unique_lock lock{mtx_};
+        if (end_flag_) {
+            throw std::runtime_error("Adding task while task queue is destroing.");
+        }
+        tasks_.emplace([task]
+        {
+            (*task)(); 
+        });
+    }
+
+    condvar_.notify_one();
+    return future;
+}
 
 #endif
