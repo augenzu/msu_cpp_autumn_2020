8beba48e391b5e5d5aa9ec3d6b94daa9d23b8791
augenzu <elfokd@gmail.com>
2020-12-03 15:11:04 +0300

    08: Add some tests

--- 08/test.cpp
+++ 08/test.cpp
@@ -1,27 +1,107 @@
 #include "thrpool.hpp"
 #include <cassert>
+#include <functional>
 #include <vector>
 
-struct A {};
+void
+void_result_test()
+{
+    struct A {};
 
-void foo(const A&) {}
+    ThreadPool pool(8);
+
+    auto task = pool.exec([](const A&){}, A());
+    task.get();
+}
 
 void
-simple_create_test()
+non_void_result_test()
 {
     ThreadPool pool(8);
 
-    auto task1 = pool.exec(foo, A());
-    task1.get();
+    auto task = pool.exec([]() 
+    { 
+        return 42; 
+    });
+    assert(task.get() == 42);
+}
 
-    int chg = 1;
-    auto task2 = pool.exec([](int chg) { chg = 3; return 42; }, chg);
-    std::cout << "task2.get(): " << task2.get() << std::endl;
-    std::cout << "chg: " << chg << std::endl;
+void
+func_with_args_test()
+{
+    ThreadPool pool(8);
+
+    int i{0};
+    double d{3.14};
+    std::string s{"str"};
+    std::vector<int> v{0, 1, 2, 3};
+    bool b{false};
+
+    auto task = pool.exec([](int i, double d, const std::string &s, 
+            const std::vector<int> &v, bool b)
+    { 
+        if (b) {
+            return v[1];
+        } else {
+            return i + s[0];
+        }
+    }, i, d, s, v, b);
+
+    assert(task.get() == i + s[0]);
+}
+
+void
+ntasks_lt_nthreads_test()
+{
+    ThreadPool pool(8);
+
+    auto duplicate = [](int i)
+    {
+        return i;
+    };
+
+    auto task0 = pool.exec(duplicate, 0);
+    auto task1 = pool.exec(duplicate, 1);
+    auto task2 = pool.exec(duplicate, 2);
+    auto task3 = pool.exec(duplicate, 3);
+    auto task4 = pool.exec(duplicate, 4);
+
+    assert(task0.get() == 0);
+    assert(task1.get() == 1);
+    assert(task2.get() == 2);
+    assert(task3.get() == 3);
+    assert(task4.get() == 4);
+}
+
+void
+ntasks_gt_nthreads_test()
+{
+    ThreadPool pool(2);
+
+    auto duplicate = [](int i)
+    {
+        return i;
+    };
+
+    auto task0 = pool.exec(duplicate, 0);
+    auto task1 = pool.exec(duplicate, 1);
+    auto task2 = pool.exec(duplicate, 2);
+    auto task3 = pool.exec(duplicate, 3);
+    auto task4 = pool.exec(duplicate, 4);
+
+    assert(task0.get() == 0);
+    assert(task1.get() == 1);
+    assert(task2.get() == 2);
+    assert(task3.get() == 3);
+    assert(task4.get() == 4);
 }
 
 const std::vector<std::function<void()>> tests{
-    simple_create_test
+    void_result_test,
+    non_void_result_test,
+    func_with_args_test,
+    ntasks_lt_nthreads_test,
+    ntasks_gt_nthreads_test
 };
 
 void
