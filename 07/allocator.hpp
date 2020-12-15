b84031f033ac31ae87ef28a057616805f1bb6c57
augenzu <elfokd@gmail.com>
2020-12-15 23:08:36 +0300

    Add construct & destroy methods

--- 07/allocator.hpp
+++ 07/allocator.hpp
@@ -2,10 +2,12 @@
 #define ALLOCATOR_HPP
 
 #include <iostream>
+#include <type_traits>
 
 template<class T>
 class Allocator
 {
+    static_assert(!std::is_same_v<T, void>, "Allocator type must be non-void.");
 public:
     using value_type = T;
 
@@ -30,6 +32,35 @@ public:
     {
         std::free(ptr);
     }
+
+    template<typename U, typename... Args>
+    U *
+    construct(U *ptr, Args &&...args)
+    {
+        auto new_obj = new (reinterpret_cast<void *>(ptr)) U{ std::forward<Args>(args)... };
+        return new_obj;
+    }
+
+    template<typename U>
+    void 
+    destroy(U *ptr)
+    {
+        ptr->~U();
+    }
 };
+
+template <class T, class U>
+bool 
+operator==(const Allocator<T> &, const Allocator<U> &) 
+{ 
+    return true;
+}
+
+template <class T, class U>
+bool 
+operator!=(const Allocator<T> &, const Allocator<U> &) 
+{ 
+    return false;
+}
 
 #endif
